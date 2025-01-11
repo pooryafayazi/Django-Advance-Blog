643568ab91db1d09ab4500fad7780ee398395be2
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-11 20:41:10 +0330

    3 apps

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,22 @@
 FROM python:3.12
+
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
-RUN mkdir /app
+
+# RUN mkdir /app
+
+# Set the working directory
 WORKDIR /app
+
+# Copy the requirements file
 COPY requirements.txt /app/
+
 RUN pip3 install --upgrade pip
+# Install dependencies
 RUN pip3 install -r requirements.txt
+
+# Copy the rest of the application code
 COPY ./core /app
 
-CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
+
+# Command to run the application
--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,13 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ye3dr4e4*qu3w_*tjg%0+tsmv!)y2d7x8j17*-_v%55i&61@5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')], default='*')
 
 # Application definition
 
@@ -37,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "accounts",
+    "blog",
+    "post",
 ]
 
 MIDDLEWARE = [
