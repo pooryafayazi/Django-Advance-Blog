87eb1651dbd52ae668af9976e3ad1996155c6c1a
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-12 13:34:20 +0330

    03.05 admin panel for User

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+from .models import User
+
+class customUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_superuser', 'is_active']
+    list_filter = ['is_superuser', 'is_active']
+    search_fields = ['email']
+    ordering = ['email']
+    fieldsets = (
+                ("Authentication", {'fields': ('email', 'password')}),
+                ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),
+                )
+    add_fieldsets = (
+                ("Authentication", {'fields': ('email', 'password1', 'password2')}),
+                ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),
+                )
+
+admin.site.register(User, customUserAdmin)
