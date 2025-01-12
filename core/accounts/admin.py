792ac861d38f855e183ee3b6c0f2bb4028193739
Poorya Fayazi <60324444+pooryafayazi@users.noreply.github.com>
2025-01-12 14:03:52 +0330

    03.06 change user admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -4,7 +4,7 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class customUserAdmin(UserAdmin):
-    model = User
+    model = User   
     list_display = ['email', 'is_superuser', 'is_active']
     list_filter = ['is_superuser', 'is_active']
     search_fields = ['email']
@@ -12,10 +12,12 @@ class customUserAdmin(UserAdmin):
     fieldsets = (
                 ("Authentication", {'fields': ('email', 'password')}),
                 ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),
+                ('groups permissions', {'fields': ('groups','user_permissions')}),
+                ('Important dates', {'fields': ('last_login',)}),
                 )
     add_fieldsets = (
-                ("Authentication", {'fields': ('email', 'password1', 'password2')}),
-                ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),
+                ('Authentication', {'fields': ('email', 'password1', 'password2')}),
+                ('Permissions', {'fields': ('is_staff','is_superuser', 'is_active')}),                
                 )
 
 admin.site.register(User, customUserAdmin)
